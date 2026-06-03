eef674b299abcbcfaf0565bbab8a22b26a4dc9a3
Rohit Sharma <rohitkumar.brainbox@gmail.com>
2026-06-03 07:26:50 +0000

    preview fixes

--- lib/carousel_slider.dart
+++ lib/carousel_slider.dart
@@ -1,7 +1,7 @@
 // Re-export carousel_slider with proper aliasing to avoid conflicts
 import 'package:carousel_slider/carousel_slider.dart' as carousel;
-import 'package:carousel_slider/carousel_controller.dart' as carousel_ctrl hide CarouselController;
-import 'package:flutter/material.dart' hide CarouselController;
+import 'package:carousel_slider/carousel_controller.dart' as carousel_ctrl;
+import 'package:flutter/material.dart' show BuildContext, Widget;
 
 // Export the carousel slider widget and options
 export 'package:carousel_slider/carousel_options.dart';
